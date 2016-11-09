898c1a8444d273e3c55f1b389c6cac5cb336c4cd
Rikard Gynnerstedt <rikard@sinch.com>
2016-11-09 14:06:12 +0100

    enabled vboxadd

--- centos-7/scripts/virtualbox.sh
+++ centos-7/scripts/virtualbox.sh
@@ -5,3 +5,5 @@ mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/isomount
 /tmp/isomount/VBoxLinuxAdditions.run
 umount /tmp/isomount
 rm -rf /tmp/isomount /home/vagrant/VBoxGuestAdditions.iso
+chkconfig --add vboxadd
+chkconfig vboxadd on
